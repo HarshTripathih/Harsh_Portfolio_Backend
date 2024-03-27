0d4b2f0dd1e46ca37fb4b779410bfc854b7fe8ea
HarshTripathih <harshtripathih321@gmail.com>
2024-03-27 12:08:26 +0530

    backend

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/database.js";
+import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 export const app = express();
 
+
+dotenv.config();
+
+// { path: "./backend/config/config.env" }
+
+connectDatabase();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -12,8 +27,15 @@ app.use(cookieParser());
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
 
-app.use(express.static(path.resolve("./frontend/build")));
+// app.use(express.static(path.resolve("./frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./frontend/build/index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("./frontend/build/index.html"));
+// });
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT} `);
 });
